587d6dbf7190a83c73d317afb57258d14933bb7e
yufcanza <4644810@gmail.com>
2026-07-10 11:25:36 +0300

    Убрала лишние закомментированные функции

--- logsan.go
+++ logsan.go
@@ -16,6 +16,8 @@ type Flags struct {
 	outDir string
 	report string
 	config string
+	inmap  string
+	outmap string
 }
 
 func main() {
@@ -36,7 +38,8 @@ func main() {
 	flag.StringVar(&flags.outDir, "out", "./clean-logs", "Выход")
 	flag.StringVar(&flags.report, "report", "report.json", "Репорт")
 	flag.StringVar(&flags.config, "config", "detectors.yaml", "Конфиг")
-
+	flag.StringVar(&flags.inmap, "inmap", "", "Загрузить словарь замен")
+	flag.StringVar(&flags.outmap, "outmap", "", "Сохранить словарь замен")
 	flag.Parse()
 	fmt.Printf("%v", flags)
 
@@ -59,109 +62,3 @@ func main() {
 
 	}
 }
-
-// reportData := &report.Report{
-// 	Detect: make(map[string]int),
-// 	Errors: []string{},
-// }
-
-// detectors, err := config.LoadConfig(flags.config)
-// if err != nil {
-// 	reportData.Errors = append(reportData.Errors,
-// 		fmt.Sprintf("Ошибка загрузки конфига: %v\n", err))
-// 	os.Exit(1)
-// }
-
-// files, err := os.ReadDir(flags.inDir)
-// if err != nil {
-// 	reportData.Errors = append(reportData.Errors,
-// 		fmt.Sprintf("Ошибка открытия каталога: %v\n", err))
-// 	os.Exit(1)
-// }
-
-// for _, file := range files {
-
-// 	inFilePath := fmt.Sprintf("%s/%s", flags.inDir, file.Name())
-// 	infile, err := os.Open(inFilePath)
-// 	if err != nil {
-// 		reportData.Errors = append(reportData.Errors,
-// 			fmt.Sprintf("Ошибка открытия файла: %v\n", err))
-// 		os.Exit(1)
-// 	}
-// 	defer infile.Close()
-
-// 	outFilePath := fmt.Sprintf("%s/clean_%s", flags.outDir, file.Name())
-// 	outfile, err := os.Create(outFilePath)
-// 	if err != nil {
-// 		reportData.Errors = append(reportData.Errors,
-// 			fmt.Sprintf("Ошибка создания выходного файла %v\n", err))
-// 		os.Exit(1)
-// 	}
-// 	defer outfile.Close()
-
-// 	reader := bufio.NewReader(infile)
-// 	writer := bufio.NewWriter(outfile)
-// 	defer writer.Flush() //все выходные данные пройдут буфер, для входных данных не требуется, обработка построчно
-
-// 	for {
-// 		line, err := reader.ReadString('\n')
-// 		if len(line) > 0 {
-// 			processed := san.ProcessLine(line, detectors)
-// 			if mode == "sanitize" {
-// 				writer.WriteString(processed)
-// 			}
-// 			if mode == "dry-run" {
-// 				fmt.Printf("dry-run: %s,%v", file.Name(), processed)
-// 			}
-// 			linecount++
-// 		}
-// 		if err != nil {
-// 			if err.Error() != "EOF" {
-// 				reportData.Errors = append(reportData.Errors,
-// 					fmt.Sprintf("Ошибка чтения файла: %v\n", err))
-// 			}
-
-// 			break
-// 		}
-
-// 	}
-// 	filecount++
-// }
-// report.CreateReport(flags.report, reportData)
-
-// if mode == "dry-run" {
-// 	reader := bufio.NewReader(infile)
-// 	linecount := 0
-// 	replacements := 0
-// 	for {
-// 		line, err := reader.ReadString('\n')
-// 		if len(line) > 0 {
-// 			processed := san.ProcessLine(line, detectors)
-// 			linecount++
-// 			if processed != line {
-// 				replacements++
-// 			}
-// 		}
-// 		if err != nil {
-// 			if err.Error() != "EOF" {
-// 				reportData.Errors = append(reportData.Errors,
-// 					fmt.Sprintf("Ошибка чтения файла: %v\n", err))
-// 			}
-
-// 			break
-// 		}
-
-// 	}
-
-// 	infile.Close()
-// 	filecount++
-
-// 	reportData.Detect = san.GetStats()
-// 	reportData.LineProc = linecount
-// 	reportData.FileProc = filecount
-
-// 	report.CreateReport(flags.report, reportData)
-// 	fmt.Print(reportData)
-// }
-
-//}
